ccb32f3064975ceaba4b0813c004e96a9a05c21d
Liam-YorkRobertson <liamyorkdeonrobertson@gmail.com>
2023-05-25 11:49:28 -0700

    created files for the specific tasks i chose

--- push.c
+++ push.c
@@ -1,11 +1,49 @@
 #include "lists.h"
 
 /**
- *
- *
- *
- *
- *
+ *p_push - adds a node to the top of the stack (push opcode)
+ *@head: double pointer to head of stack
+ *@counter: the line number
+ *Return: none (void)
  */
 
-void 
+void p_push(stack_t **head, unsigned int counter)
+{
+	int a, b = 0;
+	bool is_neg = false;
+
+	if (!trans.argu)
+	{
+		fprintf(stderr, "L%d: usage: push integer\n", counter);
+		fclose(trans.monty_file);
+		free(trans.line_content);
+		free_stack(*head);
+		exit(EXIT_FAILURE);
+	}
+	if (trans.argu[0] == "-")
+	{
+		is_neg = true;
+		b++;
+	}
+	for (; trans.argu[b] != '\0'; b++)
+	{
+		if (trans.argu[b] < '0' || trans.argu[b] > '9')
+		{
+
+			fprintf(stderr, "L%d: usage:push integer\n", counter);
+			fclose(trans.monty_file);
+			free(trans.line_content);
+			free_stack(*head);
+			exit(EXIT_FAILURE);
+		}
+	}
+	a = atoi(trans.argu);
+	if (trans.flag_change == 0)
+	{
+		add_node_to_stack(head, n);
+	}
+	else
+	{
+		add_node_to_queue(head, n);
+	}
+}
