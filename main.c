ca1183faf7c334df6acfbaa5f7336a70310a4d15
Liam-YorkRobertson <liamyorkdeonrobertson@gmail.com>
2023-05-25 09:58:22 -0700

    created and updated main.c

--- main.c
+++ main.c
@@ -9,34 +9,38 @@
 
 int main(int argc, char *argv[])
 {
+	trans_t trans = {NULL, NULL, NULL, 0};
+
 	if (argc != 2)
 	{
 		fprintf(stderr, "USAGE monty file\n");
 		exit(EXIT_FAILURE);
 	}
-	
+
 	FILE *file = fopen(argv[1], "r");
-	
-	if (!file) {
+
+	trans.monty_file = file;
+	if (!file)
+	{
 		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
 		exit(EXIT_FAILURE);
 	}
-	
+
 	char *line = NULL;
 	size_t len = 0;
 	ssize_t read;
-	
 	stack_t *stack = NULL;
 	unsigned int line_number = 0;
-	
-	while ((read = getline(&line, &len, file)) != -1) 
+
+	while ((read = getline(&line, &len, file)) != -1)
 	{
+		trans.line_content = line;
 		line_number++;
 		execute(line, &stack, line_number, file);
 	}
-	
+
 	free(line);
 	fclose(file);
 	free_stack(stack);
-	return 0;
+	return (0);
 }
