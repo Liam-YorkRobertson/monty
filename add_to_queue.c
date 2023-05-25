f54ed7a431923900a926da77b97f7a3ad3ebf2a7
Liam-YorkRobertson <liamyorkdeonrobertson@gmail.com>
2023-05-25 12:57:29 -0700

    updated files

--- add_to_queue.c
+++ add_to_queue.c
@@ -1,1 +1,40 @@
 #include "lists.h"
+
+/**
+ *add_node_to_queue - Add new node to tail of the queue
+ *@head: double pointer to head of queue
+ *@n: new value
+ *Return: none (void)
+ */
+
+void add_node_to_queue(stack_t **head, int n)
+{
+	stack_t *new_node, *current_node;
+
+	new_node = malloc(sizeof(stack_t));
+	if (new_node == NULL)
+	{
+		printf("Error: malloc failed\n");
+		return;
+	}
+
+	new_node->n = n;
+	new_node->next = NULL;
+	new_node->prev = NULL;
+
+	if (*head == NULL)
+	{
+		*head = new_node;
+	}
+	else
+	{
+		current_node = *head;
+		while (current_node->next)
+		{
+			current_node = current_node->next;
+		}
+
+		current_node->next = new_node;
+		new_node->prev = current_node;
+	}
+}
--- add_to_stack.c
+++ add_to_stack.c
@@ -1,1 +1,31 @@
 #include "lists.h"
+
+/**
+ *add_node_to_stack - adds node to the stack
+ *@head: double pointer to head of stack
+ *@n: new value
+ *Return: none (void)
+ */
+
+void add_node_to_stack(stack_t **head, int n)
+{
+	stack_t *new_node;
+
+	new_node = malloc(sizeof(stack_t));
+	if (new_node == NULL)
+	{
+		fprintf(stderr, "Error: malloc failed\n");
+		exit(EXIT_FAILURE);
+	}
+
+	new_node->n = n;
+	new_node->next = *head;
+	new_node->prev = NULL;
+
+	if (*head != NULL)
+	{
+		(*head)->prev = new_node;
+	}
+
+	*head = new_node;
+}
