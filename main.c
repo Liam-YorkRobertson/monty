d3d788a3769d120efc45f07f4ba39d25df7a49b5
Liam-YorkRobertson <liamyorkdeonrobertson@gmail.com>
2023-05-25 09:48:25 -0700

    created header file lists.h

---
+++ main.c
@@ -0,0 +1,42 @@
+#include "lists.h"
+
+/**
+ *main - interprets monty code
+ *@argc: argument count
+ *@argv: argument vector
+ *Return: 0 (success)
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argc != 2)
+	{
+		fprintf(stderr, "USAGE monty file\n");
+		exit(EXIT_FAILURE);
+	}
+	
+	FILE *file = fopen(argv[1], "r");
+	
+	if (!file) {
+		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+		exit(EXIT_FAILURE);
+	}
+	
+	char *line = NULL;
+	size_t len = 0;
+	ssize_t read;
+	
+	stack_t *stack = NULL;
+	unsigned int line_number = 0;
+	
+	while ((read = getline(&line, &len, file)) != -1) 
+	{
+		line_number++;
+		execute(line, &stack, line_number, file);
+	}
+	
+	free(line);
+	fclose(file);
+	free_stack(stack);
+	return 0;
+}
